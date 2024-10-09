7e3d3e7335c088cb2cc8a746b9471f12af45eec5
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-10-08 23:57:44 -0300

    fix: bug id

--- index.js
+++ index.js
@@ -1,76 +1,76 @@
-import express from "express";
-import { v4 as uuid } from "uuid";
-import cors from "cors";
+import express from 'express'
+import { v4 as uuid } from 'uuid'
+import cors from 'cors'
 
-import checkIdUserMiddleware from "./middlewares/checkId.js";
+import checkIdUserMiddleware from './src/middlewares/checkId.js'
 
-const app = express();
-const port = 3001;
+const app = express()
+const port = 3001
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const contacts = [];
+const contacts = []
 
 const showMethods = (request, response, next) => {
-  console.log(request.url);
-  console.log(request.method);
+  console.log(request.url)
+  console.log(request.method)
 
-  next();
-};
+  next()
+}
 
-app.post("/contacts", showMethods, (request, response) => {
-  const { contactName, phone } = request.body;
+app.post('/contacts', showMethods, (request, response) => {
+  const { contactName, phone } = request.body
   const newContact = {
-    id: uuid.v4,
+    id: uuid(),
     contactName,
     phone,
-    status: "Novo Contato adicionado",
-  };
+    status: 'Novo Contato adicionado'
+  }
 
-  contacts.push(newContact);
-  return response.status(201).json(newContact);
-});
+  contacts.push(newContact)
+  return response.status(201).json(newContact)
+})
 
-app.get("/contacts", showMethods, (request, response) => {
-  return response.json(contacts);
-});
+app.get('/contacts', showMethods, (request, response) => {
+  return response.json(contacts)
+})
 
 app.put(
-  "/contacts/:id",
+  '/contacts/:id',
   showMethods,
   checkIdUserMiddleware,
   (request, response) => {
-    const id = request.contactId;
-    const index = request.contactIndex;
-    const { contactName, phone } = request.body;
+    const id = request.contactId
+    const index = request.contactIndex
+    const { contactName, phone } = request.body
 
-    const updatedContact = { id, contactName, phone };
+    const updatedContact = { id, contactName, phone }
 
-    contacts[index] = updatedContact;
+    contacts[index] = updatedContact
 
-    return response.json(updatedContact);
+    return response.json(updatedContact)
   }
-);
+)
 
-app.delete("/contacts/:id", showMethods, (request, response) => {
-  const index = request.contactIndex;
+app.delete('/contacts/:id', showMethods, (request, response) => {
+  const index = request.contactIndex
 
-  contacts.splice(index, 1);
+  contacts.splice(index, 1)
 
-  return response.status(204).json();
-});
+  return response.status(204).json()
+})
 
 app.get(
-  "/contacts/:id",
+  '/contacts/:id',
   showMethods,
   checkIdUserMiddleware,
   (request, response) => {
-    const index = request.contactIndex;
-    return response.json(contacts[index]);
+    const index = request.contactIndex
+    return response.json(contacts[index])
   }
-);
+)
 
 app.listen(port, () => {
-  console.log(`🚀 Server started on port ${port}`);
-});
+  console.log(`🚀 Server started on port ${port}`)
+})
